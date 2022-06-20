02057f82b89fc57bf8affb6e54755eec43ac7907
rellika <rellika@lwalacommunityalliance.org>
2022-06-20 13:20:19 +0300

    Setup of error handling. Worked on Register Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,31 @@
-// import User from '../models/User.js'
+import User from '../models/User.js'
 import { StatusCodes } from 'http-status-codes'
-// import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js'
 
 const register = async (req, res) => {
 	const { name, email, password } = req.body
-
 	if (!name || !email || !password) {
-		// throw new BadRequestError('please provide all values')
-		console.log('please provide all values')
+		throw new BadRequestError('please provide all values')
+		// console.log('please provide all values')
 	}
-	res.send('Register')
+	const userAlreadyExists = await User.findOne({ email })
+	if (userAlreadyExists) {
+		throw new BadRequestError('Email already in use')
+		// console.log('Email already in use')
+	}
+	const user = await User.create({ name, email, password })
+	const token = user.createJWT()
+
+	res.status(StatusCodes.CREATED).json({
+		user: {
+			email: user.email,
+			lastName: user.lastName,
+			location: user.location,
+			name: user.name,
+		},
+		token,
+		location: user.location,
+	})
 }
 
 const login = async (req, res) => {
